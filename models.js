42c7d80eca3269179850f98282886dd301dbccaa
pedro-arruda-moreira <57548976+pedro-arruda-moreira@users.noreply.github.com>
2020-04-17 21:46:41 -0300

    remove

--- models.js
+++
@@ -1,6 +0,0 @@
-models.push(new Model(null, null, null, 'Intel'));
-models.push(new Model(null, null, null, '  Intel Core 2'));
-models.push(new Model(95, 1.3625, 3000, '    Intel Core 2 Quad Q9650'));
-models.push(new Model(null, null, null, 'AMD'));
-models.push(new Model(null, null, null, '  PileDriver'));
-models.push(new Model(220, 1.428, 4700, '    AMD FX-9590'));
